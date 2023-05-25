dbc85d4630f6bc1df0c473f0410f8024588d913b
Yong <luo.yong.name@gmail.com>
2023-05-26 06:08:40 +0800

    Update file path.

--- tools/config_extractor.el
+++ tools/config_extractor.el
@@ -1,15 +1,17 @@
 
+(require 'xml)
+
 (defun elmame-config-extractor ()
   (let (info-data)
     (save-window-excursion
       (switch-to-buffer "**mame info xml**")
       (erase-buffer)
-      (insert-file "../documents/info/mame.xml")
-      (setq info-data (libxml-parse-xml-region (point-min) (point-max)))
+      (insert-file "../documents/local/info/mame.xml")
+      (setq info-data (xml-parse-region (point-min) (point-max)))
 
       (switch-to-buffer "**mame info el**")
       (insert (format "%s" info-data))
-      (write-file "mame_info.el")
+      ;;(write-file "mame_info.el")
       )
     )
   )
